cc1d0f2a96da69cd4e3fd86bcc74f348f3cde0ec
Dede07 <154218049+DeDee07@users.noreply.github.com>
2025-10-20 13:24:03 +0100

    login

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,3 @@
-#include <raylib.h>
-#include "ball.h"
 #include "raylib.h"
 #include <iostream>
 #include <fstream>
@@ -22,12 +20,25 @@ struct Component {
 vector<Component> catalog;
 vector<Component> buildAtual;
 
-enum Screen { LOGIN, MENU, ESCOLHER_PECAS, VER_PRECOS, COMO_MONTAR };
+struct Account {
+    string username;
+    string password;
+};
+
+vector<Account> accounts = {{"admin", "Programador"}};
+
+enum Screen { LOGIN, REGISTER, MENU, ESCOLHER_PECAS, VER_PRECOS, COMO_MONTAR };
 Screen currentScreen = LOGIN;
 
 string loginUser = "";
 string loginPass = "";
 bool loginFocusUser = true;
+
+string registerUser = "";
+string registerPass = "";
+string registerPassConfirm = "";
+int registerFocusField = 0; // 0=user, 1=pass, 2=confirm
+string registerMessage = "";
 
 static inline string trim(const string &s) {
     size_t a = s.find_first_not_of(" \t\r\n");
@@ -117,11 +128,57 @@ void drawLoginScreen() {
     DrawText(maskedPass.c_str(), 210, 390, 20, BLACK);
     
     // Botão login
-    Rectangle btnLogin = {300, 460, 200, 50};
+    Rectangle btnLogin = {250, 460, 140, 50};
     DrawRectangleRec(btnLogin, DARKBLUE);
-    DrawText("ENTRAR", 355, 475, 20, WHITE);
+    DrawText("ENTRAR", 280, 475, 20, WHITE);
     
-    DrawText("Usuario: admin | Senha: Programador", 220, 550, 16, DARKGRAY);
+    // Botão criar conta
+    Rectangle btnRegister = {410, 460, 140, 50};
+    DrawRectangleRec(btnRegister, GREEN);
+    DrawText("CRIAR CONTA", 420, 475, 16, WHITE);
+    
+}
+
+void drawRegisterScreen() {
+    DrawText("=== Criar Nova Conta ===", 230, 80, 30, DARKBLUE);
+    
+    // Campo usuário
+    DrawText("Usuario:", 200, 180, 20, BLACK);
+    Rectangle userBox = {200, 210, 400, 40};
+    DrawRectangleRec(userBox, registerFocusField == 0 ? LIGHTGRAY : WHITE);
+    DrawRectangleLinesEx(userBox, 2, registerFocusField == 0 ? BLUE : GRAY);
+    DrawText(registerUser.c_str(), 210, 220, 20, BLACK);
+    
+    // Campo senha
+    DrawText("Senha:", 200, 270, 20, BLACK);
+    Rectangle passBox = {200, 300, 400, 40};
+    DrawRectangleRec(passBox, registerFocusField == 1 ? LIGHTGRAY : WHITE);
+    DrawRectangleLinesEx(passBox, 2, registerFocusField == 1 ? BLUE : GRAY);
+    string maskedPass(registerPass.length(), '*');
+    DrawText(maskedPass.c_str(), 210, 310, 20, BLACK);
+    
+    // Campo confirmar senha
+    DrawText("Confirmar Senha:", 200, 360, 20, BLACK);
+    Rectangle confirmBox = {200, 390, 400, 40};
+    DrawRectangleRec(confirmBox, registerFocusField == 2 ? LIGHTGRAY : WHITE);
+    DrawRectangleLinesEx(confirmBox, 2, registerFocusField == 2 ? BLUE : GRAY);
+    string maskedConfirm(registerPassConfirm.length(), '*');
+    DrawText(maskedConfirm.c_str(), 210, 400, 20, BLACK);
+    
+    // Mensagem de erro/sucesso
+    if (!registerMessage.empty()) {
+        Color msgColor = (registerMessage.find("sucesso") != string::npos) ? GREEN : RED;
+        DrawText(registerMessage.c_str(), 200, 450, 18, msgColor);
+    }
+    
+    // Botões
+    Rectangle btnCreate = {250, 500, 140, 50};
+    DrawRectangleRec(btnCreate, GREEN);
+    DrawText("CRIAR", 290, 515, 20, WHITE);
+    
+    Rectangle btnBack = {410, 500, 140, 50};
+    DrawRectangleRec(btnBack, GRAY);
+    DrawText("VOLTAR", 440, 515, 20, WHITE);
 }
 
 void drawMenuScreen() {
@@ -232,6 +289,30 @@ int main() {
     while (!WindowShouldClose()) {
         // Input
         if (currentScreen == LOGIN) {
+            // Detectar clique nas caixas de texto
+            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                Vector2 mousePos = GetMousePosition();
+                Rectangle userBox = {200, 280, 400, 40};
+                Rectangle passBox = {200, 380, 400, 40};
+                Rectangle btnLogin = {300, 460, 200, 50};
+                
+                // Clicar na caixa de usuário
+                if (CheckCollisionPointRec(mousePos, userBox)) {
+                    loginFocusUser = true;
+                }
+                // Clicar na caixa de senha
+                else if (CheckCollisionPointRec(mousePos, passBox)) {
+                    loginFocusUser = false;
+                }
+                // Clicar no botão de login
+                else if (CheckCollisionPointRec(mousePos, btnLogin)) {
+                    if (loginUser == "admin" && loginPass == "Programador") {
+                        currentScreen = MENU;
+                    }
+                }
+            }
+            
+            // Digitar texto
             int key = GetCharPressed();
             while (key > 0) {
                 if (key >= 32 && key <= 125) {
@@ -244,6 +325,7 @@ int main() {
                 key = GetCharPressed();
             }
             
+            // Apagar texto
             if (IsKeyPressed(KEY_BACKSPACE)) {
                 if (loginFocusUser && !loginUser.empty()) {
                     loginUser.pop_back();
@@ -252,18 +334,15 @@ int main() {
                 }
             }
             
+            // TAB para alternar entre campos
             if (IsKeyPressed(KEY_TAB)) {
                 loginFocusUser = !loginFocusUser;
             }
             
-            if (IsKeyPressed(KEY_ENTER) || IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-                Vector2 mousePos = GetMousePosition();
-                Rectangle btnLogin = {300, 460, 200, 50};
-                
-                if (CheckCollisionPointRec(mousePos, btnLogin) || IsKeyPressed(KEY_ENTER)) {
-                    if (loginUser == "admin" && loginPass == "Programador") {
-                        currentScreen = MENU;
-                    }
+            // ENTER para fazer login
+            if (IsKeyPressed(KEY_ENTER)) {
+                if (loginUser == "admin" && loginPass == "Programador") {
+                    currentScreen = MENU;
                 }
             }
         }
